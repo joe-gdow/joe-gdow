53b05e882d59d098525da397d15a19556dbc0de1
joe <joe.gdowik@gmail.com>
2022-03-09 09:43:13 -0500

    Fixes some display errors

--- calculator/script.js
+++ calculator/script.js
@@ -1,4 +1,21 @@
-const clickable = document.querySelectorAll('.clickable');
+const numbers = document.querySelectorAll('.number');
 const display = document.querySelector('.display');
-console.log(clickable);
-clickable.forEach(button => addEventListener('click', function(e) { display.innerText = e.target.innerText; }));
+const operators = document.querySelectorAll('.op');
+let num1;
+let op;
+let num2;
+
+
+numbers.forEach(button => addEventListener('click', function(e) {
+    if (e.target.classList.contains('number')) {
+        num1 = e.target.textContent;
+        display.textContent = num1;
+    }
+}));
+
+operators.forEach(button => addEventListener('click', function(e) {
+    if (e.target.classList.contains('op')) {
+        op = e.target.textContent;
+        display.textContent += op;
+    }
+}));
