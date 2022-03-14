e6ed34eeb970c23a0517a3a77042a561643864a5
joe <joe.gdowik@gmail.com>
2022-03-14 18:12:13 -0400

    Fixes event click handling using delegation.  Need to figure out how to parse the string equation and return a result

--- calculator/script.js
+++ calculator/script.js
@@ -1,21 +1,45 @@
 const numbers = document.querySelectorAll('.number');
 const display = document.querySelector('.display');
+const intDisplay = parseInt(display.innerHTML);
 const operators = document.querySelectorAll('.op');
+const calculator = document.querySelector('.calculator');
 let num1;
-let op;
 let num2;
+let op;
 
-
-numbers.forEach(button => addEventListener('click', function(e) {
-    if (e.target.classList.contains('number')) {
-        num1 = e.target.textContent;
-        display.textContent = num1;
+calculator.addEventListener('click', (e) => {
+    if (parseInt(display.innerHTML) === 0) {
+        display.innerHTML = '';
     }
-}));
-
-operators.forEach(button => addEventListener('click', function(e) {
+    if (e.target.classList.contains('number')) display.innerHTML += e.target.innerHTML;
     if (e.target.classList.contains('op')) {
-        op = e.target.textContent;
-        display.textContent += op;
+        num1 = display.innerHTML;
+        op = e.target.innerHTML;
+        display.innerHTML = num1 + op;
     }
-}));
+    if (e.target.classList.contains('equals')) {
+        let equation = Number(display.innerHTML);
+        display.innerHTML = equation;
+    }
+})
+
+/* function updateDisplay (num) {
+    if (parseInt(display.innerHTML) === 0) {
+}
+for (let button of operators) {
+    if (e.target.classList.contains('op')) {
+        addEventListener('click', function(e) {
+            let op = e.target.textContent;
+            display.textContent =  op;
+        })
+    }
+}
+
+if (parseInt(display.innerHTML) === 0) {
+    e.target.classList.contains('number') ? display.innerHTML = e.target.innerHTML : null;
+    e.target.classList.contains('op') ? display.innerHTML = e.target.innerHTML : null;
+}
+else {
+    e.target.classList.contains('number') ? display.innerHTML += e.target.innerHTML : null;
+    e.target.classList.contains('op') ? display.innerHTML += e.target.innerHTML : null;
+ } */
