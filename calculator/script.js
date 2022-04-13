3f69f4defe21b28dc011b0a21c4f3a279acd7a74
joe <joe.gdowik@gmail.com>
2022-04-13 13:05:09 -0400

    haven't committed in a while, updates incl.

--- calculator/script.js
+++ calculator/script.js
@@ -7,39 +7,29 @@ let num1;
 let num2;
 let op;
 
+const mather = {
+    '+': function(x,y) {return x + y},
+    '-': function(x,y) {return x - y},
+    '*': function(x,y) {return x * y},
+    '/': function(x,y) {return x / y},
+}
+
 calculator.addEventListener('click', (e) => {
-    if (parseInt(display.innerHTML) === 0) {
-        display.innerHTML = '';
-    }
-    if (e.target.classList.contains('number')) display.innerHTML += e.target.innerHTML;
+    if (e.target.classList.contains('number')) {
+        e.target.style.boxShadow = '2px 2px black inset';
+        display.innerHTML += e.target.innerHTML;
+    } 
     if (e.target.classList.contains('op')) {
-        num1 = display.innerHTML;
+        num1 = Number(display.innerHTML);
+        console.log(num1);
         op = e.target.innerHTML;
         display.innerHTML = num1 + op;
     }
     if (e.target.classList.contains('equals')) {
-        let equation = Number(display.innerHTML);
-        display.innerHTML = equation;
+        let num2 = parseInt(display.innerHTML.split(/[\*\/\+\-]/)[1]);//this regex searches for an operator
+        console.log('the operator is ' + op);
+        console.log('num2 is ' + num2);
+        display.innerHTML = mather[op](num1, num2);//figure out a way to take the operator out of the display string
     }
+    if (e.target.id === 'clear') {display.innerHTML = '0'}
 })
-
-/* function updateDisplay (num) {
-    if (parseInt(display.innerHTML) === 0) {
-}
-for (let button of operators) {
-    if (e.target.classList.contains('op')) {
-        addEventListener('click', function(e) {
-            let op = e.target.textContent;
-            display.textContent =  op;
-        })
-    }
-}
-
-if (parseInt(display.innerHTML) === 0) {
-    e.target.classList.contains('number') ? display.innerHTML = e.target.innerHTML : null;
-    e.target.classList.contains('op') ? display.innerHTML = e.target.innerHTML : null;
-}
-else {
-    e.target.classList.contains('number') ? display.innerHTML += e.target.innerHTML : null;
-    e.target.classList.contains('op') ? display.innerHTML += e.target.innerHTML : null;
- } */
