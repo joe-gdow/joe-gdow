d84f94ed7db44ad31d0ea20c1b937343ebc57d4c
joe_gdow <96350019+joe-gdow@users.noreply.github.com>
2022-04-20 16:38:13 -0400

    fixes multiple decimals per operand

--- calculator/script.js
+++ calculator/script.js
@@ -6,6 +6,8 @@ const intDisplay = parseInt(display.innerHTML);
 const operators = document.querySelectorAll('.op');
 const calculator = document.querySelector('.calculator');
 const clickable = document.querySelectorAll('.clickable');
+const point = document.querySelector('#point');
+
 let num1 = null;
 let num2 = null;
 let op = null;
@@ -24,11 +26,17 @@ function clear() {
     op = null;
     num1 = null;
     num2 = null;
+    point.classList.add('clickable');
+}
+
+function setPoint() {
+    point.classList.add('clickable');
 }
 
 function equals() {
     let splitDisp = display.innerHTML.split(op);
     display.innerHTML = mather[op](Number(splitDisp[0]), Number(splitDisp[1]));
+    op = null;
 }
 
 function addToDisplay(target) {
@@ -41,25 +49,29 @@ function addToDisplay(target) {
 //click event handler
 calculator.addEventListener('click', (e) => {
     if (e.target.id === 'clear') {clear()};
-    if (e.target.id === 'equals') {equals()};
-    if (e.target.id === 'point') {
-        if (display.innerHTML.search(/[.]/) === -1) {
-            display.innerHTML += e.target.innerHTML;
-        }
+    if (e.target.id === 'equals')  {
+        equals();
     }
+    if ((e.target.id === 'point') && (e.target.classList.contains('clickable'))) {
+        display.innerHTML += e.target.innerHTML;
+        e.target.classList.remove('clickable');
+    }
+
     if (e.target.classList.contains('number')) {
         addToDisplay(e.target.innerHTML);
     }
     if (e.target.classList.contains('op')) {
         if (!op) {
             op = e.target.innerHTML;
+            console.log(typeof(display.innerHTML));
             display.innerHTML += op;
-
+            setPoint();
         }
         else {
             equals();
             op = e.target.innerHTML;
             display.innerHTML += op;
+            setPoint();
         }
     }
 });
